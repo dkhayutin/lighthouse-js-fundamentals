eb57db68aad430c930990bdc29f45fc7deeff58c
Dan <dan.khayutin@gmail.com>
2018-03-23 00:50:43 +0000

    Adds looping exercises

---
+++ ingredients.js
@@ -0,0 +1,18 @@
+//while-loop
+
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var repeat = 0;
+while (repeat < ingredients.length){
+  console.log(ingredients[repeat])
+  repeat++
+}
+// for-loop
+for (var i = 0; i < ingredients.length; i++){
+  console.log(ingredients[i]);
+}
+
+// reverse for-loop
+
+for (var i = ingredients.length -1; i >=0; --i){
+  console.log(ingredients[i]);
+}
